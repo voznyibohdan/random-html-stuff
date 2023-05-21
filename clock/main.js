34f4d2c91c80cfa3ed5630044d9eadd7b587a6d0
imlaa <bohdanvoznyi.em@gmail.com>
2023-05-21 08:48:16 +0300

    cleaned console.logs

--- clock/main.js
+++ clock/main.js
@@ -4,9 +4,6 @@ const minute = document.getElementById('clock-minutes');
 const clock = () =>{
     const date = new Date();
 
-    console.log(date.getHours())
-    console.log(date.getMinutes())
-
     const hh = date.getHours() / 12 * 360;
     const mm = date.getMinutes() / 60 * 360;
 
